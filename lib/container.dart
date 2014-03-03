a8975f679a55a151ad438a3f281b49f598699e22
Dominik Martinicky <apocrypth@gmail.com>
2014-03-03 19:19:40 +0100

    almost done selector component

--- lib/container.dart
+++ lib/container.dart
@@ -43,12 +43,14 @@ class ComponentContainer extends Component {
   }
   
   render() { 
-    
+    var _items = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20, 21, 22, 23, 24, 25, 26, 27];
     return div({'style' : {'position' : 'absolute', 'right' : 0, 'left' : 0, 'top' : 0,'bottom':0},
       'onMouseMove': mouseEvent, 'onMouseUp': mouseEvent}, [
-//      sliderComponent({'minValue':10, 'maxValue':50, 'sliderWidth' : 460,
-//        'stream' : sc.stream,'barWidth' : 30},[])]);
-      selectorComponent({/*'selected':0, */'items':[0,1,2,3], 'load': load},[])    
+      sliderComponent({'minValue':10, 'maxValue':50, 'sliderWidth' : 460,
+        'stream' : sc.stream,'barWidth' : 30},[]),
+      div({'style' : {'height' : '50px'}},[]),  
+      div({'className' : 'widget widget-dark widget-full'},              //scrollStep 6,10,16 based on browser window width 
+          selectorComponent({'selected':0, 'items': _items, 'load': load, 'scrollStep' : 16},[]))
           ]); 
 
   }
--- lib/selector.dart
+++ lib/selector.dart
@@ -2,17 +2,17 @@ library selector;
 
 import 'package:react/react.dart';
 import 'package:clean_data/clean_data.dart';
-import 'dart:async';
+//import 'dart:async';
+import 'dart:html';
 
 class SelectorComponent extends Component {
-  
-//  get barWidth => props['barWidth'];
-//  get selectorWidth => props['selectorWidth'];
-  
+ 
   DataReference selected; // => props['selected'];
   var clicked;
+  
   get items => props['items'];
   get load => props['load'];
+  get scrollStep => props['scrollStep'];
   
   
   SelectorComponent() {    
@@ -26,24 +26,30 @@ class SelectorComponent extends Component {
     var _items = [];
     for (var item in items) {
       if (selected.value == item){
-        _items.add(button({'onMouseDown': (ev) => mouseDown(ev, item),'style' : {'background-color':'red'}},[item]));
+        _items.add(span({'onMouseDown': (ev) => mouseDown(ev, item),
+            'className' : 'active'},item));
       }
       else if (clicked == item){
-        _items.add(button({'onMouseDown': (ev) => mouseDown(ev, item),'style' : {'background-color':'green'}},[item]));
+        _items.add(span({'onMouseDown': (ev) => mouseDown(ev, item), 
+            'className' : 'selected'},item));
       }
       else {
-        _items.add(button({'onMouseDown': (ev) => mouseDown(ev, item),'style' : {'background-color':'#cccccc'}},[item]));  
+        _items.add(span({'onMouseDown': (ev) => mouseDown(ev, item)},item));  
       }
-      
     }
-    var leftArrowButton = button({'onMouseDown': (ev) => arrowLeftMouseDown(ev)},'<');
-    var rightArrowButton = button({'onMouseDown': (ev) => arrowRightMouseDown(ev)},'>');
+    
+    var leftArrowButton = div({'onMouseDown': (ev) => 
+        arrowLeftRightMouseDown(ev, true)},'<');
+    var rightArrowButton = div({'onMouseDown': (ev) => 
+        arrowLeftRightMouseDown(ev, false)},'>');
     
     var leftArrowDiv = div({'className' : 'left-arrow'},leftArrowButton);
-    var selectorItemsListDiv = div({'className' : 'round-list-fixed-width'}, _items);
+    var selectorItemsListDiv = div({'className' : 'round-list-fixed-width'},
+        div({'className' : 'round-list'}, _items));
     var rightArrowDiv = div({'className' : 'right-arrow'},rightArrowButton);
     
-    return div({'className' : 'round-selector round-selector-full'},[leftArrowDiv, selectorItemsListDiv, rightArrowDiv]);
+    return div({'className' : 'round-selector round-selector-full'},
+        [leftArrowDiv, selectorItemsListDiv, rightArrowDiv]);
   }
   
   mouseDown(ev, item) {
@@ -52,21 +58,52 @@ class SelectorComponent extends Component {
       if (loaded) {
         selected.value = item;
         clicked = null;
-        print(item);
         redraw();
       }
     });
     redraw();
   }
   
-  arrowLeftMouseDown(ev) {
+  arrowLeftRightMouseDown(ev,isLeft) {
+    var _scrollListDiv = querySelector('.round-list');
+    var _visibleItemsWindowSize = scrollStep * 
+        querySelector('.round-selector-full span').marginEdge.width;
+    var _scrollStep = _scrollListDiv.style.left;
+
+    if (_scrollStep == '') {
+      _scrollListDiv.style.left = '-5px';
+      _scrollStep = '-5px';
+    }
     
+    _scrollStep = _scrollStep.replaceAll('px','');
+    _scrollStep = int.parse(_scrollStep);
+
+    if (isLeft) {
+      _scrollStep += _visibleItemsWindowSize;
+    }
+    else {
+      _scrollStep -= _visibleItemsWindowSize;
+    }
+    
+    if (_scrollStep > 0){
+      _scrollStep = 0;
+    }
+    
+    if (_scrollStep < getMinMarginLeft()) {
+      _scrollStep = getMinMarginLeft();
+    }
+    
+    _scrollListDiv.style.left = '${_scrollStep}px';
     redraw();
   }
   
-  arrowRightMouseDown(ev) {
+  num getMinMarginLeft() {
+    var _itemSpan = querySelector('.round-selector-full span');
+    var _spanWidth = _itemSpan.marginEdge.width;
+    var _visibleItemsWindowSize = scrollStep * 
+        querySelector('.round-selector-full span').marginEdge.width;
     
-    redraw();
+    return (0 - _spanWidth * items.length + _visibleItemsWindowSize); 
   }
 }
 
