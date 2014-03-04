6b668a28b44e520715c07fe9368fd53ab6e6e906
Dominik Martinicky <apocrypth@gmail.com>
2014-03-04 16:07:28 +0100

    updated version of selector component (reacts to window size changes)

--- lib/container.dart
+++ lib/container.dart
@@ -2,7 +2,7 @@ library item;
 
 import 'package:react/react.dart';
 import 'package:clean_data/clean_data.dart';
-import 'dart:html';
+//import 'dart:html';
 import 'slider.dart';
 import 'selector.dart';
 import 'dart:async';
@@ -12,11 +12,11 @@ var itemList = registerComponent(() => new ComponentContainer());
 
 var sliderComponent = registerComponent(() => new SliderComponent());
 var selectorComponent = registerComponent(() => new SelectorComponent());
-
-var lastSelected;
-
 var scrollbarComponent = registerComponent(() => new ScrollbarComponent());
 
+DataReference selectorSelected;
+DataReference selectorActive;
+DataReference selectorLoading;
 
 class ComponentContainer extends Component {
 
@@ -24,33 +24,39 @@ class ComponentContainer extends Component {
 
   componentWillMount() {
     sc = new StreamController.broadcast();
+    
+    selectorSelected = new DataReference(null);
+    selectorActive = new DataReference(15);
+    selectorLoading = new DataReference(null);
+    selectorLoading.onChange.listen(load);
   }
 
   mouseEvent(ev) {
     sc.add(ev);
   }
  
-  Future load(item){
-    lastSelected = item;
+  load(item){
+    var selectorLastSelected = selectorLoading.value;
     return new Future.delayed(new Duration(seconds: 2), () {
-      if (lastSelected == item) {
-        return true;
-      }
-      else {
-        return false;
+      if (selectorLoading.value == selectorLastSelected) {
+        selectorSelected.value = selectorLastSelected;
       }
     });
   }
   
-  render() { 
+  render() {
     var _items = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20, 21, 22, 23, 24, 25, 26, 27];
     return div({'style' : {'position' : 'absolute', 'right' : 0, 'left' : 0, 'top' : 0,'bottom':0},
       'onMouseMove': mouseEvent, 'onMouseUp': mouseEvent}, [
-      sliderComponent({'minValue':10, 'maxValue':50, 'sliderWidth' : 460,
+      sliderComponent({'key' : 'slider', 'minValue':10, 'maxValue':50, 'sliderWidth' : 460,
         'stream' : sc.stream,'barWidth' : 30},[]),
-      div({'style' : {'height' : '50px'}},[]),  
-      div({'className' : 'widget widget-dark widget-full'},              //scrollStep 6,10,16 based on browser window width 
-          selectorComponent({'selected':0, 'items': _items, 'load': load, 'scrollStep' : 16},[]))
+      div({'key' : 'dummy', 'style' : {'height' : '50px'}},[]),  
+      div({'key' : 'widgetSelector', 'className' : 'widget widget-dark widget-full'},
+//scrollStep 6,10,16 based on browser window width 
+          selectorComponent({'key' : 'selector', 'selected' : selectorSelected,
+            'active' : selectorActive, 'loading': selectorLoading,
+            'items' : _items, 'scrollStep' : 10,
+            'selectorText' : 'VYBER KOLA', 'fullSize' : true},[]))
           ]); 
 
   }
--- lib/selector.dart
+++ lib/selector.dart
@@ -2,81 +2,140 @@ library selector;
 
 import 'package:react/react.dart';
 import 'package:clean_data/clean_data.dart';
-//import 'dart:async';
 import 'dart:html';
 
 class SelectorComponent extends Component {
  
-  DataReference selected; // => props['selected'];
-  var clicked;
+  DataReference get selected => props['selected'];
+  DataReference get loading => props['loading'];
+  DataReference get active => props['active'];
   
   get items => props['items'];
-  get load => props['load'];
-  get scrollStep => props['scrollStep'];
+  get selectorText => props['selectorText'];
+  get fullSize => props['fullSize'];
+  //get scrollStep => props['scrollStep'];
   
-  
-  SelectorComponent() {    
+  var scrollStep;
+  var browserWindow;
+
+
+  componentWillMount() {
+    selected.onChange.listen((_) => redraw());
+    loading.onChange.listen((_) => redraw());
+    active.onChange.listen((_) => redraw());
+    
+    browserWindow = window;
+    browserWindow.onResize.listen(checkAndSetScrollStep);
+
+    scrollStep = 16;
   }
   
-  componentWillMount() {
-    selected = new DataReference(props['selected']);
+  componentDidMount(_) {
+    setScrollStepSize();
+    
+    var _scrollListDiv = ref('round-list');
+    var _itemSpan = ref(items[0].toString());
+    var _spanWidth = _itemSpan.marginEdge.width;
+    var _visibleItemsWindowSize = scrollStep * _spanWidth;
+    var activeItemOrder = 0;
+    
+    for (int i=0; i<items.length; i++){
+      if (items[i] == active.value){
+        activeItemOrder = i;
+      }
+    }
+    var _scrollStep = (0 - (activeItemOrder * 
+                            _spanWidth - _visibleItemsWindowSize * 0.8)).round();
+    
+    checkSetScrollStepRedraw(_scrollStep, _scrollListDiv);
   }
   
   render() {
     var _items = [];
     for (var item in items) {
-      if (selected.value == item){
-        _items.add(span({'onMouseDown': (ev) => mouseDown(ev, item),
-            'className' : 'active'},item));
+      if (selected.value == item) {
+        _items.add(span({'ref' : '$item', 'key': item, 
+          'onMouseDown': (ev) => mouseDown(ev, item), 
+          'className' : 'selected'}, item));
       }
-      else if (clicked == item){
-        _items.add(span({'onMouseDown': (ev) => mouseDown(ev, item), 
-            'className' : 'selected'},item));
+      else if (loading.value == item) {
+//TODO: missing css class for loading element
+        _items.add(span({'ref' : '$item', 'key': item, 
+          'onMouseDown': (ev) => mouseDown(ev, item), 'className' : '', 
+          'style' : {'background-color' : 'red'}}, item)); 
+      }
+      else if (active.value == item) {
+        _items.add(span({'ref' : '$item', 'key': item, 
+          'onMouseDown': (ev) => mouseDown(ev, item), 
+          'className' : 'active'}, item));
       }
       else {
-        _items.add(span({'onMouseDown': (ev) => mouseDown(ev, item)},item));  
+        _items.add(span({'ref' : '$item', 'key': item, 
+          'onMouseDown': (ev) => mouseDown(ev, item)}, item));  
       }
     }
     
-    var leftArrowButton = div({'onMouseDown': (ev) => 
-        arrowLeftRightMouseDown(ev, true)},'<');
-    var rightArrowButton = div({'onMouseDown': (ev) => 
-        arrowLeftRightMouseDown(ev, false)},'>');
+    var leftArrowButton = div({'key': 'leftArrowButton', 'onMouseDown': (ev) => 
+        arrowLeftRightMouseDown(ev, true)}, '<');
+    var rightArrowButton = div({'key': 'rightArrowButton', 'onMouseDown': (ev) => 
+        arrowLeftRightMouseDown(ev, false)}, '>');
     
-    var leftArrowDiv = div({'className' : 'left-arrow'},leftArrowButton);
-    var selectorItemsListDiv = div({'className' : 'round-list-fixed-width'},
-        div({'className' : 'round-list'}, _items));
-    var rightArrowDiv = div({'className' : 'right-arrow'},rightArrowButton);
+    var textSpan = span({'key': selectorText, 
+      'className' : 'round-selector-text'}, selectorText);
+    var leftArrowDiv = div({'key': 'leftArrow', 
+      'className' : 'left-arrow'}, leftArrowButton);
+    var selectorItemsListDiv = div({'ref' : 'itemsDiv', 
+      'className' : 'round-list-fixed-width'},
+        div({'ref' : 'round-list', 'className' : 'round-list'}, _items));
+    var rightArrowDiv = div({'key': 'rightArrow', 
+      'className' : 'right-arrow'}, rightArrowButton);
     
-    return div({'className' : 'round-selector round-selector-full'},
-        [leftArrowDiv, selectorItemsListDiv, rightArrowDiv]);
+    var _cssSelectorClass = 'round-selector';
+    
+    if (fullSize){
+      _cssSelectorClass = 'round-selector round-selector-full';
+    }
+    
+    return div({'className' : _cssSelectorClass},
+        [textSpan, leftArrowDiv, selectorItemsListDiv, rightArrowDiv]);
   }
-  
-  mouseDown(ev, item) {
-    clicked = item;
-    load(item).then((loaded){
-      if (loaded) {
-        selected.value = item;
-        clicked = null;
-        redraw();
-      }
-    });
-    redraw();
-  }
-  
-  arrowLeftRightMouseDown(ev,isLeft) {
-    var _scrollListDiv = querySelector('.round-list');
-    var _visibleItemsWindowSize = scrollStep * 
-        querySelector('.round-selector-full span').marginEdge.width;
-    var _scrollStep = _scrollListDiv.style.left;
 
+  checkAndSetScrollStep(_){
+    setScrollStepSize();
+    
+    var _scrollListDiv = ref('round-list');
+    var _scrollStep = _scrollListDiv.style.left;
+    
     if (_scrollStep == '') {
       _scrollListDiv.style.left = '-5px';
       _scrollStep = '-5px';
     }
-    
+
     _scrollStep = _scrollStep.replaceAll('px','');
-    _scrollStep = int.parse(_scrollStep);
+    _scrollStep = num.parse(_scrollStep).round();
+    
+    checkSetScrollStepRedraw(_scrollStep, _scrollListDiv);
+  }
+  
+  mouseDown(ev, item) {
+    loading.value = item;
+  }
+  
+  arrowLeftRightMouseDown(ev,isLeft) {
+    var _itemSpan = ref(items[0].toString());
+    var _scrollListDiv = ref('round-list');
+    var _visibleItemsWindowSize = scrollStep * _itemSpan.marginEdge.width;
+    var _scrollStep = _scrollListDiv.style.left;
+
+    setScrollStepSize();
+    
+    if (_scrollStep == '') {
+      _scrollListDiv.style.left = '-5px';
+      _scrollStep = '-5px';
+    }
+
+    _scrollStep = _scrollStep.replaceAll('px','');
+    _scrollStep = num.parse(_scrollStep).round();
 
     if (isLeft) {
       _scrollStep += _visibleItemsWindowSize;
@@ -84,7 +143,33 @@ class SelectorComponent extends Component {
     else {
       _scrollStep -= _visibleItemsWindowSize;
     }
+
+    checkSetScrollStepRedraw(_scrollStep, _scrollListDiv);
+  }
+  
+  num getMinMarginLeft() {
+    var _itemSpan = ref(items[0].toString());
+    var _spanWidth = _itemSpan.marginEdge.width;
+    var _visibleItemsWindowSize = scrollStep * _spanWidth;
     
+    return (0 - _spanWidth * items.length + _visibleItemsWindowSize); 
+  }
+  
+  setScrollStepSize() {
+    var _itemsDivWindowSize = ref('itemsDiv').marginEdge.width;
+
+    if (_itemsDivWindowSize <= 240) { //phone size
+      scrollStep = 6;
+    }
+    else if (_itemsDivWindowSize <= 600) { //tablet size
+      scrollStep = 10;
+    }
+    else { //big size
+      scrollStep = 16;
+    }
+  }
+  
+  checkSetScrollStepRedraw(_scrollStep, _scrollListDiv){
     if (_scrollStep > 0){
       _scrollStep = 0;
     }
@@ -92,18 +177,7 @@ class SelectorComponent extends Component {
     if (_scrollStep < getMinMarginLeft()) {
       _scrollStep = getMinMarginLeft();
     }
-    
     _scrollListDiv.style.left = '${_scrollStep}px';
     redraw();
   }
-  
-  num getMinMarginLeft() {
-    var _itemSpan = querySelector('.round-selector-full span');
-    var _spanWidth = _itemSpan.marginEdge.width;
-    var _visibleItemsWindowSize = scrollStep * 
-        querySelector('.round-selector-full span').marginEdge.width;
-    
-    return (0 - _spanWidth * items.length + _visibleItemsWindowSize); 
-  }
 }
-
